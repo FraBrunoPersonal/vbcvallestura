a63d03418a74640c37fe7e299ca36f95010bf080
FraBrunoVolley <49789713+FraBrunoVolley@users.noreply.github.com>
2019-06-14 18:49:34 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,19 @@ function prev() {
  }
  slider.src = images[num];
 }
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function change() {
+  document.getElementById('prima').classList.toggle('hidden');
+  document.getElementById('dopo').classList.toggle('hidden');
+}
